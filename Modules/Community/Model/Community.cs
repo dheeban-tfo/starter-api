346a0b08a24c04931d41586dc3a112cd8bad7ad1
Dheeban <d.gnanapragasam@tfoco.com>
2024-08-29 11:05:28 +0300

    Cleaned up the folder with new project structure

---
+++ Modules/Community/Model/Community.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StarterApi.Models
+{
+    public class Community
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public List<Block> Blocks { get; set; }
+    }
+
+    public class Block
+    {
+        public int Id { get; set; }
+        public int CommunityId { get; set; }
+        public string Name { get; set; }
+        public List<Floor> Floors { get; set; }
+    }
+
+    public class Floor
+    {
+        public int Id { get; set; }
+        public int BlockId { get; set; }
+        public int FloorNumber { get; set; }
+        public List<Unit> Units { get; set; }
+    }
+
+    public class Unit
+    {
+        public int Id { get; set; }
+        public int FloorId { get; set; }
+        public string UnitNumber { get; set; }
+        public UnitType Type { get; set; }
+    }
+
+    public enum UnitType
+    {
+        Studio,
+        OneBHK,
+        TwoBHK,
+        ThreeBHK,
+        Villa
+    }
+}
