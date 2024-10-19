df579c7a69e398a2706c1f373ede4d7979440916
Dheeban <d.gnanapragasam@tfoco.com>
2024-10-19 10:13:22 +0300

    working booking and facility

--- Modules/Booking/Repositories/BookingRepository.cs
+++ Modules/Booking/Repositories/BookingRepository.cs
@@ -2,58 +2,59 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using StarterApi.Models;
 using starterapi;
+using starterapi.Services;
 
 namespace StarterApi.Repositories
 {
     public class BookingRepository : IBookingRepository
     {
-        private readonly TenantDbContext _context;
+        private readonly ITenantDbContextAccessor _contextAccessor;
         private readonly IMapper _mapper;
 
-        public BookingRepository(TenantDbContext context, IMapper mapper)
+        public BookingRepository(ITenantDbContextAccessor contextAccessor, IMapper mapper)
         {
-            _context = context;
+            _contextAccessor = contextAccessor;
             _mapper = mapper;
         }
 
         public async Task<IEnumerable<FacilityBookingDto>> GetAllAsync()
         {
-            var bookings = await _context.FacilityBookings.ToListAsync();
+            var bookings = await _contextAccessor.TenantDbContext.FacilityBookings.ToListAsync();
             return _mapper.Map<IEnumerable<FacilityBookingDto>>(bookings);
         }
 
         public async Task<FacilityBookingDto> GetByIdAsync(int id)
         {
-            var booking = await _context.FacilityBookings.FindAsync(id);
+            var booking = await _contextAccessor.TenantDbContext.FacilityBookings.FindAsync(id);
             return _mapper.Map<FacilityBookingDto>(booking);
         }
 
         public async Task<FacilityBookingDto> CreateAsync(CreateBookingDto bookingDto)
         {
             var booking = _mapper.Map<FacilityBooking>(bookingDto);
-            _context.FacilityBookings.Add(booking);
-            await _context.SaveChangesAsync();
+            _contextAccessor.TenantDbContext.FacilityBookings.Add(booking);
+            await _contextAccessor.TenantDbContext.SaveChangesAsync();
             return _mapper.Map<FacilityBookingDto>(booking);
         }
 
         public async Task<FacilityBookingDto> UpdateAsync(int id, UpdateBookingStatusDto statusDto)
         {
-            var booking = await _context.FacilityBookings.FindAsync(id);
+            var booking = await _contextAccessor.TenantDbContext.FacilityBookings.FindAsync(id);
             if (booking == null)
                 return null;
 
             _mapper.Map(statusDto, booking);
-            await _context.SaveChangesAsync();
+            await _contextAccessor.TenantDbContext.SaveChangesAsync();
             return _mapper.Map<FacilityBookingDto>(booking);
         }
 
         public async Task DeleteAsync(int id)
         {
-            var booking = await _context.FacilityBookings.FindAsync(id);
+            var booking = await _contextAccessor.TenantDbContext.FacilityBookings.FindAsync(id);
             if (booking != null)
             {
-                _context.FacilityBookings.Remove(booking);
-                await _context.SaveChangesAsync();
+                _contextAccessor.TenantDbContext.FacilityBookings.Remove(booking);
+                await _contextAccessor.TenantDbContext.SaveChangesAsync();
             }
         }
     }
--- Modules/Community/Repositories/FacilityRepository.cs
+++ Modules/Community/Repositories/FacilityRepository.cs
@@ -2,58 +2,59 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using StarterApi.Models;
 using starterapi;
+using starterapi.Services;
 
 namespace StarterApi.Repositories
 {
     public class FacilityRepository : IFacilityRepository
     {
-        private readonly TenantDbContext _context;
+        private readonly ITenantDbContextAccessor _contextAccessor;
         private readonly IMapper _mapper;
 
-        public FacilityRepository(TenantDbContext context, IMapper mapper)
+        public FacilityRepository(ITenantDbContextAccessor contextAccessor, IMapper mapper)
         {
-            _context = context;
+            _contextAccessor = contextAccessor;
             _mapper = mapper;
         }
 
         public async Task<IEnumerable<FacilityDto>> GetAllAsync()
         {
-            var facilities = await _context.Facilities.ToListAsync();
+            var facilities = await _contextAccessor.TenantDbContext.Facilities.ToListAsync();
             return _mapper.Map<IEnumerable<FacilityDto>>(facilities);
         }
 
         public async Task<FacilityDto> GetByIdAsync(int id)
         {
-            var facility = await _context.Facilities.FindAsync(id);
+            var facility = await _contextAccessor.TenantDbContext.Facilities.FindAsync(id);
             return _mapper.Map<FacilityDto>(facility);
         }
 
         public async Task<FacilityDto> CreateAsync(CreateFacilityDto facilityDto)
         {
             var facility = _mapper.Map<Facility>(facilityDto);
-            _context.Facilities.Add(facility);
-            await _context.SaveChangesAsync();
+            _contextAccessor.TenantDbContext.Facilities.Add(facility);
+            await _contextAccessor.TenantDbContext.SaveChangesAsync();
             return _mapper.Map<FacilityDto>(facility);
         }
 
         public async Task<FacilityDto> UpdateAsync(int id, UpdateFacilityDto facilityDto)
         {
-            var facility = await _context.Facilities.FindAsync(id);
+            var facility = await _contextAccessor.TenantDbContext.Facilities.FindAsync(id);
             if (facility == null)
                 return null;
 
             _mapper.Map(facilityDto, facility);
-            await _context.SaveChangesAsync();
+            await _contextAccessor.TenantDbContext.SaveChangesAsync();
             return _mapper.Map<FacilityDto>(facility);
         }
 
         public async Task DeleteAsync(int id)
         {
-            var facility = await _context.Facilities.FindAsync(id);
+            var facility = await _contextAccessor.TenantDbContext.Facilities.FindAsync(id);
             if (facility != null)
             {
-                _context.Facilities.Remove(facility);
-                await _context.SaveChangesAsync();
+                _contextAccessor.TenantDbContext.Facilities.Remove(facility);
+                await _contextAccessor.TenantDbContext.SaveChangesAsync();
             }
         }
     }
